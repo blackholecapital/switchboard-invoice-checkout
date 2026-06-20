fb1dcdf24e30f8a8727a948ca30177ffc83545a1
*** .xyz Labs *** <info@blackholecapital.xyz>
2026-06-20 10:23:50 +0200

    Refactor main.ts to use React and SuiClientProvider

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
-import { renderApp } from "./app";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-renderApp(document.getElementById("root")!);
+import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui/client";
+
+const networks = {
+  mainnet: { url: getFullnodeUrl("mainnet") }
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <SuiClientProvider networks={networks} defaultNetwork="mainnet">
+    <WalletProvider autoConnect>
+      <App />
+    </WalletProvider>
+  </SuiClientProvider>
+);
