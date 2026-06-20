05c696732fab15ded4103d64b2d8f428469e1765
*** .xyz Labs *** <info@blackholecapital.xyz>
2026-06-20 10:34:04 +0200

    Rename import from './App' to './app'

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./app";
 import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
 import { getFullnodeUrl } from "@mysten/sui/client";
 
